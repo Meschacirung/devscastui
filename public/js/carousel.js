c85de08f181eb662d6705c8ac41403aa5d261b84
MéschacIrung <meschacirung@gmail.com>
2021-03-14 10:48:28 +0200

    feat (podcasts) : create section

---
+++ public/js/carousel.js
@@ -0,0 +1,27 @@
+document.addEventListener( 'DOMContentLoaded', function () {
+    new Splide( '#podcasts', {
+
+        autoplay : false,
+        perPage : 4,
+        perMove : 1,
+        autoHeight : true,
+        pagination:false,
+        gap: 16,
+        arrows : true ,    
+        breakpoints: {
+            640: {
+                perPage: 2,
+                autoplay : false,
+            },
+            768: {
+                perPage: 4,
+                autoplay : false,
+            },
+            1024: {
+                perPage: 3,
+                autoplay : false,
+            },
+        }
+    } ).mount();
+    
+} );
