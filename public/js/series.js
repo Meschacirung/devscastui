90f0c12ff905d2ed56e3cb796701926f5afc9233
MéschacIrung <meschacirung@gmail.com>
2021-03-14 21:06:51 +0200

    feat (content-details) : create document

---
+++ public/js/series.js
@@ -0,0 +1,37 @@
+document.addEventListener( 'DOMContentLoaded', function () {
+    new Splide( '#series', {
+        autoplay : false,
+        perPage : 1,
+        perMove : 1,
+        autoHeight : true,
+        pagination:false,
+        arrows : false ,    
+        breakpoints: {
+            640: {
+                perPage: 2,
+                autoplay : false,
+            },
+            768: {
+                perPage: 4,
+                autoplay : false,
+            },
+            1024: {
+                perPage: 4,
+                autoplay : false,
+            },
+        }
+    } ).mount();
+} );
+
+
+if (window.screen.width > 1024){
+    document.querySelector(".splide").classList.remove("splide")
+    document.querySelector(".splide__track").classList.remove("splide__track")
+    document.querySelector(".splide__list").classList.remove("splide__list")
+    document.querySelector(".splide__slide").classList.remove("splide__slide")
+} else {
+    document.querySelector(".splide").classList.add("splide")
+    document.querySelector(".splide__track").classList.add("splide__track")
+    document.querySelector(".splide__list").classList.add("splide__list")
+    document.querySelector(".splide__slide").classList.add("splide__slide")
+}
